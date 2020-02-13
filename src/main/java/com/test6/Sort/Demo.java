cd980907bb226f2e386cc1b46c675ed0e8822e9b
915472537@qq.com <915472537@qq.com>
2020-02-13 23:13:38 +0800

    算法学习笔记

--- src/main/java/com/test6/Sort/Demo.java
+++ src/main/java/com/test6/Sort/Demo.java
@@ -1,5 +1,7 @@
 package com.test6.Sort;
 
+import java.util.Arrays;
+
 /**
  * @author YJ Lan
  * @create 2020-02-13-16:45
@@ -26,5 +28,33 @@ package com.test6.Sort;
     1.事后运行统计
     2.事前估算统计
  */
+/*
+    时间频度 T(n)
+    一个算法花费的时间与他执行的次数成正比，一个算法中的语句执行次数成为 语句频度 或 时间频度
+
+ */
 public class Demo {
+
+    public static void main(String[] args) {
+
+//                int[] arr = {1,9,6,3,2,8};
+
+        int[] arr = new int[100000];
+        for (int i= 0; i<100000; i++) {
+            arr[i] = (int)(Math.random() * 100000);
+        }
+        long start = System.currentTimeMillis();
+
+
+//        Sort1bubbling.sort(arr);    //冒泡排序  23s
+//        Sort1Choose.sort(arr);    //选择排序  9s
+//        Sort1Insert.sort(arr);    //插入排序  2s
+        Sort1Insert.mysort(arr);    //插入自写方法 9s
+
+
+        long end = System.currentTimeMillis();
+        long time = end - start  ;
+        System.out.println("执行时间："+time);
+//        System.out.println(Arrays.toString(arr));
+    }
 }
